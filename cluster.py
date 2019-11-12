1ee45ca7484ba434bc95cb3840efad761a865057
Boukari Yameogo <yameogo.boukari@gmail.com>
2019-11-12 18:46:01 -0500

    Commit day 2

---
+++ cluster.py
@@ -0,0 +1,253 @@
+import numpy as np
+import pandas
+from matplotlib import pyplot as plt
+import sklearn
+import matplotlib
+import urllib
+import csv
+
+import pip
+#pip.main(["install", "--upgrade", "pip"])
+
+#pip.main(["install", "--upgrade", "pyprind"])
+
+#df = pandas.read_csv("data.csv", delimiter=";")
+# lECTURE DU FICHIER SOUS FORMAT dataframe
+d = np.loadtxt("data.csv", delimiter=';')
+
+# l a3ieme colonne non utilisée dans l'algorithm
+# elle sert juste à valider les resultats
+
+
+# Analyse exploratoire-------------------------------------------------------
+# Affichage des données
+print(d)
+y=d[:,2]
+print(np.unique(y))
+
+#print(df.head(3))
+
+#from matplotlib import pyplot as plt
+
+plt.figure(figsize=(10,8))
+
+for clusterId, color in zip(range(0, 5), ('grey','blue', 'red', 'orange', 'brown')):
+    plt.scatter(x=d[:,0][d[:,2]== clusterId],
+                y=d[:,1][d[:,2]== clusterId],
+                color=color,
+                marker='o',
+                alpha=0.6
+                )
+plt.title('Clustered Dataset', size=20)
+plt.xlabel('$x_1$', size=25)
+plt.xlabel('$x_2$', size=25)
+
+plt.tick_params(axis='both', which='major', labelsize=18)
+plt.xlim(xmin=np.amin(d[:,0])-0.5, xmax=np.amax(d[:,0])+0.5)
+plt.xlim(xmin=np.amin(d[:,1])-0.5, xmax=np.amax(d[:,1])+0.5)
+
+plt.show()
+
+import pyprind
+
+
+
+# ------------------- DEBUT DE L'ALGO -------------------------
+#Step 1: Calculating density at each point
+
+# Calcue de la distance
+def euclidean_dist (p,q):
+    return (np.sqrt((p[0]-q[0])**2 +(p[1]-q[1])**2 ))
+
+
+
+
+
+
+# Calcul de la densité
+def cal_density (d , dc=0.1):
+    n=d.shape[0] # taille de la matrice
+    den_arr = np.zeros(n, dtype=np.int) # Definition d'un matrice vide
+
+    for i in range(n):
+        for j in range(i+1,n):
+            if euclidean_dist(d[i,:], d[j,:]) < dc:
+                den_arr[i]+=1
+                den_arr[j] += 1
+
+    return (den_arr)
+
+den_arr=cal_density(d[:,0:2], 0.5)
+
+print (max(den_arr)) # la valeurs maximum
+#print("den_arr=",den_arr )
+
+
+
+
+
+
+
+#Step 2: Calculating the minimum distance to high density points
+
+def cal_minDist2Peaks(d,den_arr):
+    n=d.shape[0]
+    mdist2peaks = np.repeat(999,n)
+    max_pdist = 0 #store the maximum pairwise distance
+    for i in range(n):
+        mdist_i=mdist2peaks[i]
+        for j in range(i+1,n):
+            dist_ij=euclidean_dist(d[i,0:2], d[j,0:2])
+            max_pdist=max(max_pdist, dist_ij)
+            if den_arr[i] < den_arr[j]:
+                mdist_i=min(mdist_i, dist_ij)
+            elif den_arr[j] <= den_arr[i]:
+                mdist2peaks[j]=min(mdist2peaks[j], dist_ij)
+        mdist2peaks[i]=mdist_i
+
+    #update the value for the point with hightest density
+    max_den_points= np.argwhere(mdist2peaks == 999)
+    print(max_den_points)
+    mdist2peaks[max_den_points]=max_pdist
+    return (mdist2peaks)
+
+#Finding the number of clusters
+
+mdist2peaks =cal_minDist2Peaks(d,den_arr)
+
+#Plotting the decision graph
+
+def plot_decisionGraph(den_arr,mdist2peaks, thresh=None):
+
+    plt.figure(figsize=(10,8))
+
+    if thresh is not None:
+        centroids = np.argwhere((mdist2peaks > thresh) & (den_arr > 1)).flatten()
+        noncenter_points = np.argwhere((mdist2peaks < thresh))
+    else:
+        centroids= None
+        noncenter_points=np.arange(den_arr.shape[0])
+
+    plt.scatter(x=den_arr[noncenter_points],
+                y=mdist2peaks[noncenter_points],
+                color='blue',
+                marker='o',
+                alpha=0.5,
+                s=50)
+
+    if thresh is not None:
+        plt.scatter(x=den_arr[centroids],
+                    y=mdist2peaks[centroids],
+                    color='red',
+                    marker='o',
+                    alpha=0.6,
+                    s=140)
+
+    plt.title('Decision Graph', size=20)
+    plt.xlabel(r'$\rho$', size=25)
+    plt.ylabel(r'$\delta$', size=25)
+    plt.ylim(ymin=min(mdist2peaks-0.5), ymax=max(mdist2peaks+0.5))
+    plt.xlim(xmin=min(den_arr - 0.5), xmax=max(den_arr + 0.5))
+
+    plt.tick_params(axis='both', which='major', labelsize=18)
+    plt.show()
+
+plot_decisionGraph(den_arr, mdist2peaks,1.7)
+
+
+
+
+
+
+
+
+# Calcul des centres des classe
+#Cluster centroids
+
+thresh=1.0 # hyperparamètres
+centroids = np.argwhere((mdist2peaks > thresh) & (den_arr>1)).flatten()
+
+print(centroids.reshape(1, centroids.shape[0]))
+
+plt.figure(figsize=(10,8))
+
+d_centers = d[centroids,:]
+
+for clustId,color in zip(range(0,5),('grey', 'blue', 'red', 'orange', 'brown')):
+    plt.scatter(x=d[:,0][d[:,2] == clustId],
+                y=d[:,1][d[:,2] == clustId],
+                color='grey',
+                marker='o',
+                alpha=0.4
+                )
+    # plot the cluster centroids
+    plt.scatter(x=d_centers[:,0][d_centers[:,2] == clustId],
+                y=d_centers[:,1][d_centers[:,2] == clustId],
+                color='purple',
+                marker='*',
+                s=400,
+                alpha=1.0
+                )
+plt.title('Cluster Centroids', size=20)
+plt.xlabel('$x_1$', size=25)
+plt.ylabel('$x_2$', size=25)
+
+plt.tick_params(axis='both', which='major', labelsize=18)
+plt.xlim(xmin=min(d[:,0])-1, xmax=max(d[:,0])+1)
+plt.ylim(ymin=min(d[:,1])-1, ymax=max(d[:,1])+1)
+
+plt.show()
+
+
+def assign_cluster(df, den_arr, centroids):
+    """ Assign points to clusters
+    """
+    nsize = den_arr.shape[0]
+    #print (nsize)
+    cmemb = np.ndarray(shape=(nsize,2), dtype='int')
+    cmemb[:,:] = -1
+    ncm = 0
+    for i,cix in enumerate(centroids):
+        cmemb[i,0] = cix # centroid index
+        cmemb[i,1] = i   # cluster index
+        ncm += 1
+    da = np.delete(den_arr, centroids)
+    inxsort = np.argsort(da)
+    for i in range(da.shape[0]-1, -1, -1):
+        ix = inxsort[i]
+        dist = np.repeat(999.9, ncm)
+        for j in range(ncm):
+            dist[j] = euclidean_dist(df[ix], df[cmemb[j,0]])
+            #print(j, ix, cmemb[j,0], dist[j])
+        nearest_nieghb = np.argmin(dist)
+        cmemb[ncm,0] = ix
+        cmemb[ncm,1] = cmemb[nearest_nieghb, 1]
+        ncm += 1
+    return(cmemb)
+
+clust_membership = assign_cluster(d, den_arr, centroids)
+
+
+plt.figure(figsize=(10,8))
+
+for clustId,color in zip(range(0,5),('grey', 'blue', 'red', 'orange', 'brown')):
+    cset = clust_membership[clust_membership[:,1] == clustId,0]
+    plt.scatter(x=d[cset,0],
+                y=d[cset,1],
+                color=color,
+                marker='o',
+                alpha=0.6
+                )
+plt.title('Clustered Dataset by Density-Peak Algorithm', size=20)
+plt.xlabel('$x_1$', size=25)
+plt.ylabel('$x_2$', size=25)
+
+plt.tick_params(axis='both', which='major', labelsize=18)
+plt.xlim(xmin=np.amin(d[:,0])-1, xmax=np.amax(d[:,0])+1)
+plt.ylim(ymin=np.amin(d[:,1])-1, ymax=np.amax(d[:,1])+1)
+
+plt.show()
+
+
+
+#from matplotlib import pyplot as plt
---
+++ clustering_project/Clusterinr_1_2.py
@@ -0,0 +1,113 @@
+#####################################################
+# Author: Boukari Yameogo                           #
+# Date: 2019-11-05                                  #
+# This class is to verify if the dataset meets      #
+# the clustering conditions                         #
+#####################################################
+
+import numpy as np
+import pandas as pd
+
+
+def timing(func):
+    """
+    Cette fonction est un decorateur qui permet de calculer le temps d'execution d'une fonction
+    :param func: la fonction a évaluer
+    :return: exécute la fonction et le temps d'exécution
+    """
+    import time
+
+    def wrapper(*args, **kwargs):
+        print("Calculation start")
+        start = time.perf_counter()
+        res = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        print(f"Execution time: {round(end_time-start,2)} second(s)")
+        print("Finished!")
+        return res
+    return wrapper
+
+
+class Process:
+    """
+    Clustering algorithm to optimize the
+    """
+    def __init__(self, df, d_min):
+        """
+        Initialisation des paramètres à la construction de la classe
+        :param df: un dataframe de type pandas
+        :param d_min: la distance minimale (hyperparamètre)
+        """
+        self.df = df
+        self.d_min = d_min
+        self.nbr_points = self.df.shape[0]
+        self.nbr_dim = self.df.shape[1]
+        self.densite = []
+        self.pos = []
+        self.eps = 1e-20 # valeur d'epsilon pour comparer à 0
+
+    def dist(self, point_a, point_b):
+        """
+        Fonction de calcul de la distance euclidienne entre 2 points
+        :param point_a: le point b (pourrai correspondre à une observation dans un dataframe)
+        :param point_b: le point b (pourrai correspondre à une observation dans un dataframe)
+        :return: Retourne la distance euclidienne entre les 2 points point_a et point_b
+        """
+        dimension = self.nbr_dim
+        total = 0
+        for i in range(dimension):
+            total += (point_a[i] - point_b[i])**2
+        return np.sqrt(total)
+
+    def func_densite(self):
+        """
+        Fonction de calcul du nombre point situé dans un rayon de d_min d chaque point
+        Ici le point pour lequel le calcul est fait n'est pas inclu
+        :return: Retourne un liste contenant la densité pour chaque point
+        """
+        dmin = self.d_min
+        n = self.nbr_points
+        dens = np.zeros(n, dtype=int) # on defini un liste
+        for i in range(n):
+            a = self.df.iloc[i, :]
+            for j in range(i+1, n):
+                b = self.df.iloc[j, :]
+                if self.dist(point_a=a, point_b=b) < dmin:
+                    dens[i] += 1
+                    dens[j] += 1
+        return dens
+
+    def comptage(self, array, val):
+        """
+        Fonction de comptage d'un nombre dans un array
+        :param array: array dans lequel la recherche doit s'effectuer
+        :param val: la valeur dont on veut compter l'occurence
+        :return: Retourne le nombre total d'occurence de val dans array
+        """
+        n = len(array)
+        nbr = 0
+        for i in range(n):
+            if (array[i] - val) > self.eps:
+                nbr += 1
+        return nbr
+
+    # def peak(self):
+    #     rhos = self.func_densite()
+    #     n = len(rhos)
+    #     peak = list(np.zeros(n, dtype=int))
+    #     unique_val = np.unique(rhos)
+    #     for i in unique_val:
+
+
+@timing
+def main():
+    df = pd.read_csv("data.csv", sep=";", header=None, usecols=[0, 1])
+    p = Process(df, 0.5)
+    dens = p.func_densite()
+    print(f"Dimension de df: {df.shape}\n")
+    print(f"Nombre de valeur de densité: {len(dens)}\n")
+    print(dens)
+
+
+if __name__ == "__main__":
+    main()
